8a761bf67fa6e96004d215453cb417e6a5cea752
Nobriga20 <courtvizn@gmail.com>
2025-01-04 13:27:42 -0800

    next push

--- index.js
+++ index.js
@@ -2,6 +2,32 @@
 // service_ba8q94k
 // VOMXoZRy_Hu0gZMmY
 
+let isModalOpen = false;
+let contrastToggle = false
+const scalefactor = 1/20
+
+function moveBackGround(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x=event.clientx * scalefactor;
+    const y=event.clienty * scalefactor;
+    
+    for (let i=0; i < shapes.length; ++i) {
+        const isodd = i % 2 !== 0
+        const boolInt = isodd ? -1 : 1;
+        shapes[i].style.transform = 'translate(${x * boolInt}px, ${y * boolInt}px)'
+    }
+}
+
+function toggleContrast() {
+    contrastToggle =!contrastToggle
+  if (contrastToggle) {
+    document.body.classlist += " dark-theme"
+}
+else {
+    document.body.ClassList.remove("dark-theme")
+}
+}
+
 function contact(event) {
    event.PreventDefault();
    const loading = document.querySelector('.modal__overlay--loading')
@@ -12,7 +38,7 @@ function contact(event) {
        'service_ba8q94k',
         'template_u2jv7vp',
         event.target,
-        'VOMXoZRy_Hu0gZMmY'
+        '7W0h4pZMuuMIZ-nvh'
     ) .then(() => {
         loading.classList.remove("modal__overlay--visible")
         success.classlist += "modal__overlay--visible";
@@ -24,7 +50,7 @@ function contact(event) {
    })
 }
 
-let isModalOpen = false;
+
 function toggleModal () {
     if (ismodalOpen) {
         isModalOpen = false
